a2ea0ae713af72c174a68c2b008b58a1bb4f63ae
Patman1O1 <patrickthomas101x@gmail.com>
2025-10-24 11:21:02 -0500

    Arduino needed car_control.ino in it's own folder

---
+++ car_control/car_control.ino
@@ -0,0 +1,23 @@
+// C/C++ Includes
+#include <cstddef>
+
+// Arduino Includes
+#include <Arduino.h>
+#include <Serial.h>
+
+// Local Includes
+#include "DigitalPin.hpp"
+#include "AnalogPin.hpp"
+
+ls
+
+void setup(void) noexcept {
+  Serial.begin(9600);
+
+}
+
+
+void loop(void) noexcept {
+  
+
+}
