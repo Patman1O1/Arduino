683f966c78bbc6ff8410d46c3e1adee5c41b54e5
Patrick Thomas <48868595+Patman1O1@users.noreply.github.com>
2025-10-24 14:42:38 -0500

    Update DCMotor.cpp

--- car_control/DCMotor.cpp
+++ car_control/DCMotor.cpp
@@ -17,12 +17,18 @@ namespace arduino {
 
   void DCMotor::setAnalogPin(AnalogPin&& analogPin) noexcept { this->analogPin = analogPin; }
 
+  // TODO
+  void DCMotor::setSpeed(std::uint32_t speed) noexcept {
+      //
+  }
+
   /* ----------------------------------------------------Getters-------------------------------------------------- */
   // TODO
-  [[nodiscard]] std::uint32_t DCMotor::getRPM(void) const noexcept {
+  [[nodiscard]] std::uint32_t DCMotor::getSpeed(void) const noexcept {
       return 0;
   }
 
 } // namespace arduino
+
 
 
